3d5e1fb0269a4cbf380f9c2ce5cebcb99afbfebb
= <=>
2023-04-09 16:27:33 -0700

    First-Class Function

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction (){
+return function namedFunction() {
+    return here;
+  
+}
+
+}
+
+ function returnsAnAnonymousFunction(){
+    return function(){
+        return here;
+    }
+
+ }
